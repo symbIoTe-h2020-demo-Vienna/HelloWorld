86e7807d4ab61b7907d6107169b686a4aaca8dc2
vthglyk <vasgl@intracom-telecom.com>
2016-08-11 13:18:29 +0300

    Added comment

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     LocalTime currentTime = new LocalTime();
     System.out.println("\nThe current local time is: " + currentTime);
 
+    // Create the Greeter
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
