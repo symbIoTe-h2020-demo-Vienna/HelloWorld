283c755f27fb8ef162fb6d01b6277455c8f1b4f2
vthglyk <vasgl@intracom-telecom.com>
2016-08-25 09:58:26 +0300

    added develop to safelist

--- merge_script.sh
+++ merge_script.sh
@@ -4,6 +4,7 @@ fi
 
 echo $TRAVIS_BRANCH
 echo $TRAVIS_REPO_SLUG
+git branch
 git checkout develop 
 git merge "$TRAVIS_BRANCH" 
 git push --repo https://travis-symbiote:Travis1@github.com/symbiote-h2020/HelloWorld.git
