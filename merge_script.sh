c7a28ce81f27ba2f8700df30fa46938282122531
vthglyk <vasgl@intracom-telecom.com>
2016-08-25 09:43:55 +0300

    merge_script.sh v2

--- merge_script.sh
+++ merge_script.sh
@@ -2,7 +2,8 @@ if [ "$TRAVIS_BRANCH" != "staging" ]; then
     exit 0;
 fi
 
-
+echo $TRAVIS_BRANCH
+echo $TRAVIS_REPO_SLUG
 git checkout develop 
-git merge "$TRAVIS_COMMIT" 
+git merge "$TRAVIS_BRANCH" 
 git push --repo https://travis-symbiote:Travis1@github.com/symbiote-h2020/HelloWorld.git
