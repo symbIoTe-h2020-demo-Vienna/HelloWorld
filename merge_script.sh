4a83e830cd69844eb4865e2e0bdb508a264954e4
vthglyk <vasgl@intracom-telecom.com>
2016-08-25 10:08:15 +0300

    merge v2

--- merge_script.sh
+++ merge_script.sh
@@ -4,6 +4,7 @@ fi
 
 echo $TRAVIS_BRANCH
 echo $TRAVIS_REPO_SLUG
+git fetch https://travis-symbiote:Travis1@github.com/symbiote-h2020/HelloWorld.git develop
 git branch
 git checkout develop 
 git merge "$TRAVIS_BRANCH" 
