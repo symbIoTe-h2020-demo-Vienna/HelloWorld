98bc3ba43ceba244e05841de45afddbfe72fb2f5
vthglyk <vasgl@intracom-telecom.com>
2016-08-25 15:11:20 +0300

    added sleep

--- merge_script.sh
+++ merge_script.sh
@@ -44,6 +44,8 @@ printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
 #push_uri="https://travis-symbiote:Travis1@github.com/$GITHUB_REPO"
 
+sleep 30
+
 # Redirect to /dev/null to avoid secret leakage
 printf 'git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1'
 git push "$push_uri" "$BRANCH_TO_MERGE_INTO" 
